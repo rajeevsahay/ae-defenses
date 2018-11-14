4f9d2962e960ac7aa4ab152ce2c6309292aaf5b1
rajeevsahay <44882360+rajeevsahay@users.noreply.github.com>
2018-11-14 17:15:25 -0500

    Add files via upload

---
+++ Semi-white_Box_Attack_(FGM)/data_collection_fgm_wb.py
@@ -0,0 +1,132 @@
+import numpy as np
+import keras
+from keras import backend
+from keras.datasets import mnist
+from keras.models import load_model
+from keras import backend as K
+from cleverhans.utils_keras import KerasModelWrapper
+from cleverhans_class import FastGradientMethod
+
+
+e = np.linspace(0.05,3.5,70)
+data = np.zeros([70, 16])
+
+#Load training and testing data and normalize to [0, 1]
+(data_train, labels_train), (data_test, labels_test) = mnist.load_data()
+data_train = data_train/255.0
+data_test = data_test/255.0
+
+#Flatten dataset (New shape for training and testing set is (60000,784) and (10000, 784))
+data_train = data_train.reshape((len(data_train), np.prod(data_train.shape[1:])))
+data_test = data_test.reshape((len(data_test), np.prod(data_test.shape[1:])))
+
+#Create labels as one-hot vectors
+labels_train = keras.utils.np_utils.to_categorical(labels_train, num_classes=10)
+labels_test = keras.utils.np_utils.to_categorical(labels_test, num_classes=10)
+
+#Import trained classifer
+backend.set_learning_phase(False)
+fc_classifier = load_model('fc-100-100-10.h5')
+
+#Evaluate on clean data
+scores = fc_classifier.evaluate(data_test, labels_test)
+
+#Setup adversairal attack
+sess =  backend.get_session()
+ord = 2
+wrap = KerasModelWrapper(fc_classifier)
+fgm = FastGradientMethod(wrap, sess=sess)
+
+#Load pre-processing DAE
+pp_ae_linf_eps25 = load_model('../Semi-white_Box_Attack_(FGSM)/pp_auto_encoder_fgsm.h5')
+
+#Load dimensionality reduction AE
+red_dim_ae_20 = load_model('../saved_models/autoencoders/784-20-784_100epochs.h5')
+red_dim_ae_40 = load_model('../saved_models/autoencoders/784-40-784_100epochs.h5')
+red_dim_ae_60 = load_model('../saved_models/autoencoders/784-60-784_100epochs.h5')
+red_dim_ae_80 = load_model('../saved_models/autoencoders/784-80-784_100epochs.h5')
+red_dim_ae_100 = load_model('../saved_models/autoencoders/784-100-784_100epochs.h5')
+red_dim_ae_331 = load_model('../saved_models/autoencoders/784-331-784_100epochs.h5')
+
+#Indicate how to obtain hidden representation of AE
+ae_hidden_representation_20 = K.function([red_dim_ae_20.layers[0].input], [red_dim_ae_20.layers[0].output])
+ae_hidden_representation_40 = K.function([red_dim_ae_40.layers[0].input], [red_dim_ae_40.layers[0].output])
+ae_hidden_representation_60 = K.function([red_dim_ae_60.layers[0].input], [red_dim_ae_60.layers[0].output])
+ae_hidden_representation_80 = K.function([red_dim_ae_80.layers[0].input], [red_dim_ae_80.layers[0].output])
+ae_hidden_representation_100 = K.function([red_dim_ae_100.layers[0].input], [red_dim_ae_100.layers[0].output])
+ae_hidden_representation_331 = K.function([red_dim_ae_331.layers[0].input], [red_dim_ae_331.layers[0].output])
+
+#Load classifier with input dimension corresponding to AE
+red_dim_classifier_20 = load_model('../saved_models/classifiers/20-100_100epochs.h5')
+red_dim_classifier_40 = load_model('../saved_models/classifiers/40-100_100epochs.h5')
+red_dim_classifier_60 = load_model('../saved_models/classifiers/60-100_100epochs.h5')
+red_dim_classifier_80 = load_model('../saved_models/classifiers/80-100_100epochs.h5')
+red_dim_classifier_100 = load_model('../saved_models/classifiers/100-100_100epochs.h5')
+red_dim_classifier_331 = load_model('../saved_models/classifiers/331-100_100epochs.h5')
+
+for idx, epsilon in enumerate(e):
+    print(idx)
+    data[idx, 0] = epsilon
+    data[idx, 1] = scores[1]*100 #No attack, no defense
+
+    #Create adversarial examples according to l-2 norm on testing data
+    adv_test_x = fgm.generate_np(data_test, eps=epsilon, ord=ord, clip_min=0., clip_max=1.)
+
+    #Evaluate model after attacking data without pre-processing
+    adv_acc = fc_classifier.evaluate(adv_test_x, labels_test)
+    data[idx, 2] = adv_acc[1]*100 #attack, no defense
+
+    #Run testing data through DAE pre-processor
+    denoised_data = pp_ae_linf_eps25.predict(adv_test_x)
+
+    #Evaluate accuracy of classifier after pre-processing
+    adv_scores = fc_classifier.evaluate(denoised_data, labels_test)
+    data[idx, 3] = adv_scores[1]*100 #attacked and defended using DAE trained on eps=0.25
+
+    #Obtain reduced dimensions of perturbed testing set
+    hidden_representation_20 = ae_hidden_representation_20([adv_test_x])
+    hidden_representation_40 = ae_hidden_representation_40([adv_test_x])
+    hidden_representation_60 = ae_hidden_representation_60([adv_test_x])
+    hidden_representation_80 = ae_hidden_representation_80([adv_test_x])
+    hidden_representation_100 = ae_hidden_representation_100([adv_test_x])
+    hidden_representation_331 = ae_hidden_representation_331([adv_test_x])
+
+    #Evaluate accuracy with reduced dimensions
+    red_dim_acc_20 = red_dim_classifier_20.evaluate(hidden_representation_20, labels_test)
+    data[idx,4] = red_dim_acc_20[1]*100 #attacked and defended using reduced dimension of 20
+    red_dim_acc_40 = red_dim_classifier_40.evaluate(hidden_representation_40, labels_test)
+    data[idx,5] = red_dim_acc_40[1]*100 #attacked and defended using reduced dimension of 40
+    red_dim_acc_60 = red_dim_classifier_60.evaluate(hidden_representation_60, labels_test)
+    data[idx,6] = red_dim_acc_60[1]*100 #attacked and defended using reduced dimension of 60
+    red_dim_acc_80 = red_dim_classifier_80.evaluate(hidden_representation_80, labels_test)
+    data[idx,7] = red_dim_acc_80[1]*100 #attacked and defended using reduced dimension of 80
+    red_dim_acc_100 = red_dim_classifier_100.evaluate(hidden_representation_100, labels_test)
+    data[idx,8] = red_dim_acc_100[1]*100 #attacked and defended using reduced dimension of 100
+    red_dim_acc_331 = red_dim_classifier_331.evaluate(hidden_representation_331, labels_test)
+    data[idx,9] = red_dim_acc_331[1]*100 #attacked and defended using reduced dimension of 331
+
+
+    #Cascade defenses
+    series_defense_data_20 = ae_hidden_representation_20([denoised_data])
+    sereis_acc_20 = red_dim_classifier_20.evaluate(series_defense_data_20, labels_test)
+    data[idx,10] = sereis_acc_20[1]*100 #attacked and defended using series defense with DAE trained on eps=0.25 and K=20
+    series_defense_data_40 = ae_hidden_representation_40([denoised_data])
+    sereis_acc_40 = red_dim_classifier_40.evaluate(series_defense_data_40, labels_test)
+    data[idx,11] = sereis_acc_40[1]*100 #attacked and defended using series defense with DAE trained on eps=0.25 and K=40
+    series_defense_data_60 = ae_hidden_representation_60([denoised_data])
+    sereis_acc_60 = red_dim_classifier_60.evaluate(series_defense_data_60, labels_test)
+    data[idx,12] = sereis_acc_60[1]*100 #attacked and defended using series defense with DAE trained on eps=0.25 and K=60
+    series_defense_data_80 = ae_hidden_representation_80([denoised_data])
+    sereis_acc_80 = red_dim_classifier_80.evaluate(series_defense_data_80, labels_test)
+    data[idx,13] = sereis_acc_80[1]*100 #attacked and defended using series defense with DAE trained on eps=0.25 and K=80
+    series_defense_data_100 = ae_hidden_representation_100([denoised_data])
+    sereis_acc_100 = red_dim_classifier_100.evaluate(series_defense_data_100, labels_test)
+    data[idx,14] = sereis_acc_100[1]*100 #attacked and defended using series defense with DAE trained on eps=0.25 and K=100
+    series_defense_data_331 = ae_hidden_representation_331([denoised_data])
+    sereis_acc_331 = red_dim_classifier_331.evaluate(series_defense_data_331, labels_test)
+    data[idx,15] = sereis_acc_331[1]*100 #attacked and defended using series defense with DAE trained on eps=0.25 and K=331
+
+np.savetxt("fgm_whitebox_using_linf_dae.csv", data, delimiter=",")
+
+
+
---
+++ Semi-white_Box_Attack_(FGM)/pp_auto_encoder_fgm_l2.py
@@ -0,0 +1,93 @@
+import numpy as np
+from keras.models import Sequential
+from keras.datasets import mnist
+from keras.layers import Dense
+from keras.models import load_model
+from keras import backend
+import matplotlib.pyplot as plt
+from cleverhans_class import FastGradientMethod
+from cleverhans.utils_keras import KerasModelWrapper
+
+#Loss minimizes to 0.0035 over 100 epochs
+
+
+#Load MNIST data and normalize to [0,1]
+(data_train, labels_train), (data_test, labels_test) = mnist.load_data()
+data_train = data_train/255.0
+data_test = data_test/255.0
+
+#Flatten dataset (New shape for training and testing set is (60000,784) and (10000, 784))
+data_train = data_train.reshape((len(data_train), np.prod(data_train.shape[1:])))
+data_test = data_test.reshape((len(data_test), np.prod(data_test.shape[1:])))
+
+#Load classifier model whose gradients will be used to create adversarial examples
+keras_model = load_model('fc-100-100-10.h5')
+backend.set_learning_phase(False)
+
+#Create adversarial examples on testing data
+sess =  backend.get_session()
+epsilon = 1.0
+ord = 2
+wrap = KerasModelWrapper(keras_model)
+fgsm = FastGradientMethod(wrap, sess=sess)
+adv_train_x = fgsm.generate_np(data_train, eps=epsilon, ord=ord, clip_min=0., clip_max=1.)
+adv_test_x = fgsm.generate_np(data_test, eps=epsilon, ord=ord, clip_min=0., clip_max=1.)
+
+
+#Total datasets consisting of clean and corrupted data
+data_total_train = np.vstack([data_train, adv_train_x])
+data_total_test = np.vstack([data_test, adv_test_x])
+
+#Create labels that correspond to clean reconstructions
+labels_total_train = np.vstack([data_train, data_train])
+labels_total_test = np.vstack([data_test, data_test])
+
+#Create the model
+def autoencoder():
+
+    model = Sequential()
+    model.add(Dense(256, activation=None, use_bias=True, kernel_initializer="uniform", input_dim=784))
+    model.add(Dense(128, activation=None, kernel_initializer="uniform"))
+    model.add(Dense(64, activation=None, kernel_initializer="uniform"))
+    model.add(Dense(128, activation=None, kernel_initializer="uniform"))
+    model.add(Dense(256, activation=None, kernel_initializer="uniform"))
+    model.add(Dense(784, activation="sigmoid", kernel_initializer="uniform"))
+    return model
+
+
+
+model = autoencoder()
+
+#Compile model uisng mean squared error as loss and adam as optimizer
+model.compile(loss='mean_squared_error', optimizer='adam')
+
+#Train model using input of clean and corrupted data and fit to clean reconstructions only
+model.fit(data_total_train, labels_total_train, validation_data=(data_total_test, labels_total_test), epochs=100, batch_size=256, shuffle=True)
+
+#Save the model
+model.save('pp_auto_encoder_fgm_l2_eps1.h5')
+
+#Predict reconstructions of test data
+decoded_images = model.predict(data_total_test)
+
+#Plot samples of first 15 perturbed images before and after reconstruction
+n = 15  # how many digits we will display
+plt.figure(figsize=(20, 4))
+for i in range(n):
+    # display original
+    ax = plt.subplot(2, n, i + 1)
+    plt.imshow(data_total_test[i+10000].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # display reconstruction
+    ax = plt.subplot(2, n, i + 1 + n)
+    plt.imshow(decoded_images[i+10000].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.show()
+
+
+
