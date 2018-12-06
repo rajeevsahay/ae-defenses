18683eddeb7004b8a42ba21046ce311befa5f546
rajeevsahay <44882360+rajeevsahay@users.noreply.github.com>
2018-12-06 14:42:30 -0500

    Add files via upload

---
+++ Mismatch_attack_(FGM)/bl_box_fgm.py
@@ -0,0 +1,94 @@
+import numpy as np
+import keras
+from keras.datasets import mnist
+from keras.models import load_model
+from keras import backend
+from keras import backend as K
+from cleverhans.attacks import FastGradientMethod
+from cleverhans.utils_keras import KerasModelWrapper
+#from cleverhans_class import FastGradientMethod
+
+
+#Load training and testing data and normalize in [0, 1]
+(data_train, labels_train), (data_test, labels_test) = mnist.load_data()
+data_train = data_train/255.0
+data_test = data_test/255.0
+
+#Flatten dataset (New shape for training and testing set is (60000,784) and (10000, 784))
+data_train = data_train.reshape((len(data_train), np.prod(data_train.shape[1:])))
+data_test = data_test.reshape((len(data_test), np.prod(data_test.shape[1:])))
+
+#Create labels as one-hot vectors
+labels_train = keras.utils.np_utils.to_categorical(labels_train, num_classes=10)
+labels_test = keras.utils.np_utils.to_categorical(labels_test, num_classes=10)
+
+#Import trained classifer
+fc_classifier = load_model('fc-100-100-10.h5')
+
+#Evaluate on clean data
+scores = fc_classifier.evaluate(data_test, labels_test)
+
+#Print accuracy of unattacked, no defense testing set
+print("Accuracy of clean data without any defense")
+print ("Accuracy: %.2f%%" %(scores[1]*100))
+
+
+#Create adversarial examples on testing data using gradients from mismatched classifier
+atk_classifier = load_model('fc-200-200-100-10.h5')
+backend.set_learning_phase(False)
+sess =  backend.get_session()
+epsilon = 1.0
+wrap = KerasModelWrapper(atk_classifier)
+fgsm = FastGradientMethod(wrap, sess=sess)
+#adv_train_x = fgsm.generate_np(data_train, eps=epsilon, clip_min=0., clip_max=1.)
+adv_test_x = fgsm.generate_np(data_test, eps=epsilon, ord=2, clip_min=0., clip_max=1.)
+
+
+
+#Evaluate model after attacking data
+adv_acc = fc_classifier.evaluate(adv_test_x, labels_test)
+
+#Print accuracy of attacked, no defense testing set
+print("Accuracy after attack, no defense")
+print ("Accuracy: %.2f%%" %(adv_acc[1]*100))
+
+#Load pre-processing autoencoder trained using adversarial examples from original classifier
+pp_ae = load_model('pp_auto_encoder_mismatch_linf.h5')
+#pp_ae = load_model('../Semi-white_Box_Attack_(FGSM)/pp_auto_encoder_fgsm.h5')
+
+#Run testing data through pre-processor
+decoded_data = pp_ae.predict(adv_test_x)
+
+#Evaluate accuracy of classifier after pre-processing
+adv_scores = fc_classifier.evaluate(decoded_data, labels_test)
+
+#Print accuracy of attacked data after preprocessing
+print("Accuracy of perturbed data using DAE (trained with actual classifier for l-inf attack) as Defense")
+print ("Accuracy: %.2f%%" %(adv_scores[1]*100))
+
+#Load dimensionality reduction AE
+red_dim_ae = load_model('../saved_models/autoencoders/784-331-784_100epochs.h5')
+
+#Obtain reduced dimensions of perturbed testing set
+ae_hidden_representation = K.function([red_dim_ae.layers[0].input], [red_dim_ae.layers[0].output])
+hidden_representation = ae_hidden_representation([adv_test_x])
+
+#Load classifier with input dimension of 331
+red_dim_classifier = load_model('../saved_models/classifiers/331-100_100epochs.h5')
+
+#Evaluate accuracy with reduced dimensions
+red_dim_acc = red_dim_classifier.evaluate(hidden_representation, labels_test)
+
+#Print accuracy of attacked data after reducing dimensions
+print("Accuracy of perturbed data using dim. red. as Defense")
+print ("Accuracy: %.2f%%" %(red_dim_acc[1]*100))
+
+#Cascade defense
+series_defense_data = ae_hidden_representation([decoded_data])
+sereis_acc = red_dim_classifier.evaluate(series_defense_data, labels_test)
+
+#Print accuracy of attacked data after series defense
+print("Accuracy of perturbed data using series Defense")
+print ("Accuracy: %.2f%%" %(sereis_acc[1]*100))
+
+
---
+++ Mismatch_attack_(FGM)/fc_classifier_100_100_10.py
@@ -0,0 +1,59 @@
+#Creating a fully-connected classifier using clean,
+#uncorrupted MNIST data on ten digits
+
+#Loss minimizes to 3.2291e-6 over 100 epochs using cross entropy and adam
+#Acieves accuracy of 97.88% on testing data
+
+import numpy as np
+from keras.models import Sequential
+import keras
+from keras.datasets import mnist
+from keras.layers import Dense
+
+
+#Import dataset and normalize to [0,1]
+(data_train, labels_train), (data_test, labels_test) = mnist.load_data()
+data_train = data_train/255.0
+data_test = data_test/255.0
+
+#Flatten dataset (New shape for training and testing set is (60000,784) and (10000, 784))
+data_train = data_train.reshape((len(data_train), np.prod(data_train.shape[1:])))
+data_test = data_test.reshape((len(data_test), np.prod(data_test.shape[1:])))
+
+#Create labels as one-hot vectors
+labels_train = keras.utils.np_utils.to_categorical(labels_train, num_classes=10)
+labels_test = keras.utils.np_utils.to_categorical(labels_test, num_classes=10)
+
+
+#Create the model
+def fc_model():
+
+    model = Sequential()
+    model.add(Dense(100, activation="relu", use_bias=True, kernel_initializer="normal", input_dim=784))
+    model.add(Dense(100, activation="relu", kernel_initializer="normal"))
+    model.add(Dense(10, activation="softmax", kernel_initializer="normal"))
+    return model
+
+
+
+model = fc_model()
+
+#Model parameters
+batch_size = 200
+
+#Compile model using croo entropy as loss and adam as optimizer
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+#Train model using input of clean and corrupted data and fit to clean reconstructions only
+model.fit(data_train, labels_train, validation_data=(data_test, labels_test), epochs=100, batch_size=batch_size, shuffle=True)
+
+
+#Save the model
+model.save('fc-100-100-10.h5')
+
+#Evaluate
+scores = model.evaluate(data_test, labels_test)
+
+#Print accuracy
+print ("Accuracy: %.2f%%" %(scores[1]*100))
+
